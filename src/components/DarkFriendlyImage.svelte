dae30ad003e2d60dd60c38a90dc8c11d700e336c
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-11-03 20:17:09 +0000

    Add dark mode friendly images

---
+++ src/components/DarkFriendlyImage.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let src: string;
+    export let alt: string;
+
+    $: darkSrc = src.replace(/(\.[\w\d_-]+)$/i, "-dark$1");
+    $: lightSrc = src.replace(/(\.[\w\d_-]+)$/i, "-light$1");
+</script>
+
+<div>
+    <img
+        class="hidden dark:block"
+        src={darkSrc}
+        alt={`${alt} (dark mode friendly)`}
+    />
+    <img
+        class="dark:hidden"
+        src={lightSrc}
+        alt={`${alt} (light mode friendly)`}
+    />
+</div>
