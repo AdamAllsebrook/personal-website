9aca5e941752f06a86d6b67b3c2d007da2ef7e70
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-11-04 13:33:35 +0000

    Better code snippet component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,54 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = {
-    base: {
-        black: '#100F0F',
-        950: '#1C1B1A',
-        900: '#282726',
-        850: '#343331',
-        800: '#403E3C',
-        700: '#575653',
-        600: '#6F6E69',
-        500: '#878580',
-        300: '#B7B5AC',
-        200: '#CECDC3',
-        150: '#DAD8CE',
-        100: '#E6E4D9',
-        50: '#F2F0E5',
-        paper: '#FFFCF0',
-    },
-    red: {
-        DEFAULT: '#AF3029',
-        light: '#D14D41',
-    },
-    orange: {
-        DEFAULT: '#BC5215',
-        light: '#DA702C',
-    },
-    yellow: {
-        DEFAULT: '#AD8301',
-        light: '#D0A215',
-    },
-    green: {
-        DEFAULT: '#66800B',
-        light: '#879A39',
-    },
-    cyan: {
-        DEFAULT: '#24837B',
-        light: '#3AA99F',
-    },
-    blue: {
-        DEFAULT: '#205EA6',
-        light: '#4385BE',
-    },
-    purple: {
-        DEFAULT: '#5E409D',
-        light: '#8B7EC8',
-    },
-    magenta: {
-        DEFAULT: '#A02F6F',
-        light: '#CE5D97',
-    },
-};
+import { colors } from './colors'
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -65,7 +16,7 @@ export default {
                         '--tw-prose-body': theme('colors.base.black'),
                         '--tw-prose-headings': theme('colors.purple.DEFAULT'),
                         '--tw-prose-invert-body': theme('colors.base.paper'),
-                        '--tw-prose-invert-headings': theme('colors.purple.light'),
+                        '--tw-prose-invert-headings': theme('colors.orange.light'),
                     }
                 }
             }),
