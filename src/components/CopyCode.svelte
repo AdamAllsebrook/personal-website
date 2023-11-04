32ea6345149303756c529a99157873d35c3ff0ff
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-11-04 15:15:33 +0000

    Rename code block to code snippet, add copy button

---
+++ src/components/CopyCode.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { Copy, Check } from "lucide-svelte";
+    import { scale } from "svelte/transition";
+
+    export let code: string;
+
+    let copied = false;
+
+    const copyCode = () => {
+        navigator.clipboard.writeText(code);
+        copied = true;
+        setTimeout(() => {
+            copied = false;
+        }, 1000);
+    };
+</script>
+
+<button
+    on:click={copyCode}
+    class="absolute right-0 p-2 opacity-0 group-hover:opacity-100 transition-opacity grid"
+    style={copied ? "opacity: 1" : ""}
+>
+    {#if copied}
+        <div
+            class="col-start-1 row-start-1"
+            transition:scale={{ duration: 300 }}
+        >
+            <Check size="24" class="text-base-200 dark:text-base-800" />
+        </div>
+    {:else}
+        <div
+            class="col-start-1 row-start-1"
+            transition:scale={{ duration: 300 }}
+        >
+            <Copy size="24" class="text-base-200 dark:text-base-800" />
+        </div>
+    {/if}
+</button>
