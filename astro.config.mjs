8c59907d711dba399e8c51a89506cd58d7d6dae5
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-11-04 15:55:07 +0000

    Add reading time to posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import { remarkReadingTime } from './src/remark-reading-time.mjs';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), mdx()]
+    integrations: [tailwind(), svelte(), mdx()],
+    markdown: {
+        remarkPlugins: [remarkReadingTime]
+    }
 });
