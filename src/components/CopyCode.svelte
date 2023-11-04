23f4bcaf509fb822e4ef3deb0b24e9a534806e83
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-11-04 15:37:44 +0000

    Format all files with prettier

--- src/components/CopyCode.svelte
+++ src/components/CopyCode.svelte
@@ -1,38 +1,32 @@
 <script lang="ts">
-    import { Copy, Check } from "lucide-svelte";
-    import { scale } from "svelte/transition";
+  import { Copy, Check } from "lucide-svelte";
+  import { scale } from "svelte/transition";
 
-    export let code: string;
+  export let code: string;
 
-    let copied = false;
+  let copied = false;
 
-    const copyCode = () => {
-        navigator.clipboard.writeText(code);
-        copied = true;
-        setTimeout(() => {
-            copied = false;
-        }, 1000);
-    };
+  const copyCode = () => {
+    navigator.clipboard.writeText(code);
+    copied = true;
+    setTimeout(() => {
+      copied = false;
+    }, 1000);
+  };
 </script>
 
 <button
-    on:click={copyCode}
-    class="absolute right-0 p-4 opacity-0 group-hover:opacity-100 transition-opacity grid"
-    style={copied ? "opacity: 1" : ""}
+  on:click={copyCode}
+  class="absolute right-0 grid p-4 opacity-0 transition-opacity group-hover:opacity-100"
+  style={copied ? "opacity: 1" : ""}
 >
-    {#if copied}
-        <div
-            class="col-start-1 row-start-1"
-            transition:scale={{ duration: 300 }}
-        >
-            <Check size="24" class="text-base-200 dark:text-base-800" />
-        </div>
-    {:else}
-        <div
-            class="col-start-1 row-start-1"
-            transition:scale={{ duration: 300 }}
-        >
-            <Copy size="24" class="text-base-200 dark:text-base-800" />
-        </div>
-    {/if}
+  {#if copied}
+    <div class="col-start-1 row-start-1" transition:scale={{ duration: 300 }}>
+      <Check size="24" class="text-base-200 dark:text-base-800" />
+    </div>
+  {:else}
+    <div class="col-start-1 row-start-1" transition:scale={{ duration: 300 }}>
+      <Copy size="24" class="text-base-200 dark:text-base-800" />
+    </div>
+  {/if}
 </button>
--- src/components/DarkModeToggle.svelte
+++ src/components/DarkModeToggle.svelte
@@ -1,47 +1,41 @@
 <script lang="ts">
-    import { Sun } from "lucide-svelte";
-    import { Moon } from "lucide-svelte";
-    import { onMount } from "svelte";
-    import { scale } from "svelte/transition";
-    import { darkMode } from "../stores";
+  import { Sun } from "lucide-svelte";
+  import { Moon } from "lucide-svelte";
+  import { onMount } from "svelte";
+  import { scale } from "svelte/transition";
+  import { darkMode } from "../stores";
 
-    let htmlRoot: HTMLElement | null;
+  let htmlRoot: HTMLElement | null;
 
-    onMount(() => {
-        htmlRoot = document.querySelector("html");
-    });
+  onMount(() => {
+    htmlRoot = document.querySelector("html");
+  });
 
-    const setDarkMode = (value: boolean) => {
-        if (htmlRoot) {
-            htmlRoot.classList.toggle("dark", value);
-        }
-    };
+  const setDarkMode = (value: boolean) => {
+    if (htmlRoot) {
+      htmlRoot.classList.toggle("dark", value);
+    }
+  };
 
-    const toggleDarkMode = () => {
-        $darkMode = !$darkMode;
-        setDarkMode($darkMode);
-    };
+  const toggleDarkMode = () => {
+    $darkMode = !$darkMode;
+    setDarkMode($darkMode);
+  };
 </script>
 
 <button class="grid" name="toggleDarkMode" on:click={() => toggleDarkMode()}>
-    {#if $darkMode === true}
-        <div
-            class="col-start-1 row-start-1"
-            transition:scale={{ duration: 300 }}
-        >
-            <Moon size="24" strokeWidth="1.5" />
-        </div>
-    {:else if $darkMode === false}
-        <div
-            class="col-start-1 row-start-1"
-            transition:scale={{ duration: 300 }}
-        >
-            <Sun size="24" strokeWidth="1.5" />
-        </div>
-    {:else}
-        <div class="opacity-0">
-            <Moon size="24" strokeWidth="1.5" />
-        </div>
-    {/if}
+  {#if $darkMode === true}
+    <div class="col-start-1 row-start-1" transition:scale={{ duration: 300 }}>
+      <Moon size="24" strokeWidth="1.5" />
+    </div>
+  {:else if $darkMode === false}
+    <div class="col-start-1 row-start-1" transition:scale={{ duration: 300 }}>
+      <Sun size="24" strokeWidth="1.5" />
+    </div>
+  {:else}
+    <div class="opacity-0">
+      <Moon size="24" strokeWidth="1.5" />
+    </div>
+  {/if}
 </button>
 <label for="toggleDarkMode" class="hidden">Toggle Dark Mode</label>
